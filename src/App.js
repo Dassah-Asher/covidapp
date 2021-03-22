0fa47519a75754dbfaec603c3bb1f6a7b65daf32
Asher <ashernuo07@gmail.com>
2021-03-22 12:34:41 +0000

    aa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
+import  Stats from "./components/Stats"
+import CountrySelector from "./components/CountrySelector"
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      
+      <CountrySelector/>
+      <h3>Global Summary</h3>
+      <Stats url="https://covid19.mathdro.id/api"/>
+     
+    
     </div>
   );
 }
---
+++ src/components/useStats.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from "react"
+function useStats(url) {
+   const[stats, setStats]= useState();
+   const[error, setError]= useState();
+   const[loading, setloading]= useState(true);
+
+   useEffect(()=>{
+      const fetchData = async ()=>{
+          const data = await fetch(url)
+          .then((res)=>res.json())
+          .catch((err)=>setError(err))
+          setStats(data);
+          setloading(false);
+
+      };
+      fetchData()
+   },[url]);
+   return{
+       stats,
+        error,
+     loading,
+   }
+
+}
+
+export default useStats;
